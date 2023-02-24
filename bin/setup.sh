a410f99cc47ac96f35156e6d83895a5a0bcad23d
CF <captainfedora@skillup.tech>
2023-02-24 07:48:32 -0800

    fixing setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,25 +3,18 @@ echo "****************************************"
 echo " Setting up Capstone Environment"
 echo "****************************************"
 
-echo "Updating package manager..."
-sudo add-apt-repository -y ppa:deadsnakes/ppa
-
-echo "Installing Python 3.9 and Virtual Environment"
+echo "Installing Python 3.8 Virtual Environment"
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.9 python3.9-venv
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.8-venv
 
-echo "Making Python 3.9 the default..."
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
+echo "Making Python 3.8 the default..."
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 
 echo "Checking the Python version..."
 python3 --version
 
 echo "Creating a Python virtual environment"
-python3 -m venv venv
-
-echo  "Copying the ~/.bashrc file for backup"
-cp ~/.bashrc ~/.bashrc.bak
+python3 -m venv ~/backend-pics-venv
 
 echo "Configuring the developer environment..."
 echo "# Backend Capstone Project additions" >> ~/.bashrc
@@ -29,8 +22,9 @@ echo "export GITHUB_ACCOUNT=$GITHUB_ACCOUNT" >> ~/.bashrc
 echo 'export PS1="\[\e]0;\u:\W\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ "' >> ~/.bashrc
 
 echo "Installing Python depenencies..."
-source ./venv/bin/activate && python3 -m pip install --upgrade pip wheel
-source ./venv/bin/activate && pip install -r requirements.txt
+source ~/backend-pics-venv/bin/activate && python3 -m pip install --upgrade pip wheel
+source ~/backend-pics-venv/bin/activate && pip install -r requirements.txt
+echo "source ~/backend-pics-venv/bin/activate" >> ~/.bashrc
 
 echo "****************************************"
 echo " Capstone Environment Setup Complete"
